f1575de94fe1bd238f19d97ae554eb1d5ba24f2c
Mark <mark@remarkablemark.org>
2024-11-17 16:08:35 -0500

    refactor(lifecycles): update text to "LittleJS Template"

--- src/lifecycles/gameRenderPost.ts
+++ src/lifecycles/gameRenderPost.ts
@@ -4,7 +4,7 @@ import { vec2 } from 'littlejsengine';
 export function gameRenderPost() {
   // draw to overlay canvas for hud rendering
   LittleJS.drawTextScreen(
-    'LittleJS with TypeScript',
+    'LittleJS Template',
     vec2(LittleJS.mainCanvasSize.x / 2, 80),
     80,
   );
